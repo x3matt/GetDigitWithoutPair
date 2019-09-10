d36f3995b0cb87b45205812702f612ee90eb6925
x3matt <x3matt21@mail.ru>
2019-09-11 04:23:44 +0700

    Added solution

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,2 +1,20 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestClass {
+
+    GetDigitWithoutPairClass clazz = new GetDigitWithoutPairClass();
+
+    @Test
+    public void testGetDigitWithoutPair(){
+        assertEquals(9,clazz.getDigitWithoutPair(new int[]{1,6,7,1,7,9,6}));
+        assertEquals(7,clazz.getDigitWithoutPair(new int[]{1,1,7,2,2,8}));
+        assertEquals(5,clazz.getDigitWithoutPair(new int[]{1,2,5,4,3,6}));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testGetDigitWithoutPair_NullPointerException(){
+        clazz.getDigitWithoutPair(null);
+    }
 }
